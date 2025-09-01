6bdc1d1a67286e2bec275e6a0cb9ba1e37641395
Marjolein <barendse@uoregon.edu>
2025-09-01 10:41:06 +0200

    Addd data preparation script

--- collect_eddyqc_params.sh
+++ collect_eddyqc_params.sh
@@ -17,7 +17,7 @@ for subject in `cat $subjectlist` ; do
 subject_folder=$subject
 cd $processed_data_folder/${subject_folder}
 
-#${subject}_eddycor.eddy_parameters = trans_x trans_y trans_z rot_x rot_y rot_z ec_x ec_y ec_z ec_quad[xyz] ec_mult[xyx]
+#${subject}.eddy_parameters = trans_x trans_y trans_z rot_x rot_y rot_z ec_x ec_y ec_z ec_quad[xyz] ec_mult[xyx]
 #${subject}.eddy_movement_rms = abs_mot rel_mot
 #${subject}.eddy_restricted_movement_rms = (ignores PE direction) abs_restrict_mot rel_restrict_mot
 #${subject}.eddy_outlier_n_stdev_map = how many SDs off the mean difference between observation and prediction is. remove first line, take average abs value across columns(=slices) and minimum and maximum values across columns
@@ -30,7 +30,9 @@ sed '1d' ${subject}.eddy_outlier_n_stdev_map | awk  '{b=0; for (i=1;i<=NF;i++) i
 sed '1d' ${subject}.eddy_outlier_map | awk '{sum = 0; for (i = 1; i <= NF; i++) sum += $i; print sum}' > ${subject}_outliers.txt 
 
 #tSNR
-fslmaths ${subject}_b0s.nii.gz -mas ${subject}_b0_brain_mask.nii.gz -Tstd ${subject}_b0_std.nii.gz 
+fslmaths ${subject}_unwarped_b0s -Tmean ${subject}_b0_mean
+bet ${subject}_b0_mean ${subject}_b0_brain -m -f 0.3
+fslmaths ${subject}_unwarped_b0s.nii.gz -mas ${subject}_b0_brain_mask.nii.gz -Tstd ${subject}_b0_std.nii.gz 
 fslmaths ${subject}_b0_mean.nii.gz -div ${subject}_b0_std.nii.gz ${subject}_tsnr.nii.gz
 tsnr=`fslstats ${subject}_tsnr.nii.gz -M`
 nvols=`cat ${subject}.eddy_parameters | wc -l`
